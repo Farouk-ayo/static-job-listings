fee6a020f1966de51513285142287f36ae7cd5a2
frkayo <mustaphafarouk41@gmail.com>
2022-10-28 18:24:37 +0100

    not hardcoded anymore

--- code/src/components/UI/Card.js
+++ code/src/components/UI/Card.js
@@ -1,119 +1,44 @@
-import photosnap from "../../images/photosnap.svg";
-import manage from "../../images/manage.svg";
-import account from "../../images/account.svg";
-import myHome from "../../images/myhome.svg";
 import classes from "./Card.module.css";
+import DUMMY_jOBS from "../store/cartContext";
 
 const Card = () => {
-  return (
-    <section className={classes.card}>
+  const allJobs = [];
+
+  DUMMY_jOBS.forEach((job) => {
+    allJobs.push(
       <main className={`${classes.eachCard} `}>
-        <div className={classes.span}></div>
+        {job.featured && job.new && <div className={classes.span}></div>}
+
         <div className={classes.cardTitle}>
-          <img src={photosnap} alt="" srcset="" />
+          <img src={job.image} alt="" srcset="" />
           <div className={classes.info}>
             <div className={classes.headlines}>
-              <p>Photosnap</p>
-              <h2 className={classes.new}>NEW!</h2>
-              <h2 className={classes.featured}>FEATURED</h2>
+              <p>{job.name}</p>
+              {job.new && <h2 className={classes.new}>{job.new}</h2>}
+              {job.featured && (
+                <h2 className={classes.featured}>{job.featured}</h2>
+              )}
             </div>
-            <h1>Senior Frontend Developer</h1>
+            <h1>{job.title}</h1>
             <div className={classes.description}>
-              <p>1d ago</p>
+              <p>{job.day}</p>
               <span>.</span>
-              <p>Full Time</p>
+              <p>{job.duration}</p>
               <span>.</span>
-              <p>USA only</p>
+              <p>{job.location}</p>
             </div>
           </div>
         </div>
         <div className={classes.caption}>
-          <caption>Frontend</caption>
-          <caption>Senior</caption>
-          <caption>HTML</caption>
-          <caption>css</caption>
-          <caption>Javascript</caption>
+          {job.captions.map((caption) => (
+            <caption>{caption}</caption>
+          ))}
         </div>
       </main>
-      <main className={`${classes.eachCard} `}>
-        <div className={classes.span}></div>
-        <div className={classes.cardTitle}>
-          <img src={manage} alt="" srcset="" />
-          <div className={classes.info}>
-            <div className={classes.headlines}>
-              <p>Manage</p>
-              <h2 className={classes.new}>NEW!</h2>
-              <h2 className={classes.featured}>FEATURED</h2>
-            </div>
-            <h1>Fullstack Developer</h1>
-            <div className={classes.description}>
-              <p>1d ago</p>
-              <span>.</span>
-              <p>Part Time</p>
-              <span>.</span>
-              <p>Remote</p>
-            </div>
-          </div>
-        </div>
-        <div className={classes.caption}>
-          <caption>Fullstack</caption>
-          <caption>Midweight</caption>
-          <caption>Python</caption>
-          <caption>React</caption>
-        </div>
-      </main>
-      <main className={`${classes.eachCard} `}>
-        <div className={classes.cardTitle}>
-          <img src={account} alt="" srcset="" />
-          <div className={classes.info}>
-            <div className={classes.headlines}>
-              <p>Account</p>
-              <h2 className={classes.new}>NEW!</h2>
-            </div>
-            <h1>Junior Frontend Developer</h1>
-            <div className={classes.description}>
-              <p>2d ago</p>
-              <span>.</span>
-              <p>Part Time</p>
-              <span>.</span>
-              <p>USA only</p>
-            </div>
-          </div>
-        </div>
-        <div className={classes.caption}>
-          <caption>Frontend</caption>
-          <caption>Junior</caption>
-          <caption>React</caption>
-          <caption>Sass</caption>
-          <caption>Javascript</caption>
-        </div>
-      </main>
-      <main className={`${classes.eachCard} `}>
-        <div className={classes.cardTitle}>
-          <img src={myHome} alt="" srcset="" />
-          <div className={classes.info}>
-            <div className={classes.headlines}>
-              <p>MyHome</p>
-            </div>
-            <h1>Junior Frontend Developer</h1>
-            <div className={classes.description}>
-              <p>5d ago</p>
-              <span>.</span>
-              <p>Contract</p>
-              <span>.</span>
-              <p>USA only</p>
-            </div>
-          </div>
-        </div>
-        <div className={classes.caption}>
-          <caption>Frontend</caption>
-          <caption>Junior</caption>
-          <caption>CSS</caption>
-          <caption>JavaScript</caption>
-        </div>
-      </main>
-    </section>
-  );
+    );
+  });
+
+  return <section className={classes.card}>{allJobs}</section>;
 };
 
 export default Card;
--- code/src/components/store/cartContext.js
+++ code/src/components/store/cartContext.js
@@ -1,1 +1,54 @@
-const DUMMY_jOBS = [{}];
+import photosnap from "../../images/photosnap.svg";
+import manage from "../../images/manage.svg";
+import account from "../../images/account.svg";
+import myHome from "../../images/myhome.svg";
+
+const DUMMY_jOBS = [
+  {
+    id: 1,
+    name: "Photosnap",
+    image: photosnap,
+    new: "NEW!",
+    featured: "FEATURED",
+    title: "Senior Frontend Developer",
+    day: "1d ago",
+    duration: "Full Time",
+    location: "USA only",
+    captions: ["Frontend", "Senior", "HTML", "css", "Javascript"],
+  },
+  {
+    id: 2,
+    name: " Manage",
+    image: manage,
+    new: "NEW!",
+    featured: "FEATURED",
+    title: "Fullstack Developer",
+    day: "1d ago",
+    duration: "Part Time",
+    location: "Remote",
+    captions: ["Fullstack", "Midweight", "Python", "React"],
+  },
+  {
+    id: 3,
+    name: " Account",
+    image: account,
+    new: "NEW!",
+    title: "Junior Frontend Developer",
+    day: "2d ago",
+    duration: "Part Time",
+    location: "USA only",
+    captions: ["Frontend", "Junior", "React", "Sass", "Javascript"],
+  },
+  {
+    id: 4,
+    name: " MyHome",
+    image: myHome,
+    title: "Junior Frontend Developer",
+    day: "5d ago",
+    duration: "Contract",
+    location: "USA only",
+    captions: ["Frontend", "Junior", "CSS", "JavaScript"],
+  },
+];
+
+export default DUMMY_jOBS;
